a85f93b4dbf3d28446f344ce8bf26800af52ddd0
Josh Ospina <Josh.999126@gmail.com>
2017-10-19 14:25:31 -0500

    Installing CSS Dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <v-app>
+      <main>
+        <v-container> Hello World </v-container>
+      </main>
+    </v-app>
   </div>
 </template>
 
