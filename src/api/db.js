37725307c56aa2f49d219e5bf9717a4a1f542d01
Josh Ospina <Josh.999126@gmail.com>
2017-10-19 05:37:02 -0500

    Creation of User's Endpoints

--- src/api/db.js
+++ src/api/db.js
@@ -1,23 +1,38 @@
 import { db } from './firebase'
 
 const usersRef = db.collection('users')
-const tasksRef = db.collection('tasks')
+/* const tasksRef = db.collection('tasks')  In Progress */
 
 export default {
-  postUser (userObj) {
+  createUser (userObj) {
     const user = {
       id: userObj.id,
       name: userObj.name,
       lastName: userObj.lastName,
       email: userObj.email,
-      isAdmin: false
+      isAdmin: false,
+      isActive: true
     }
 
     usersRef.doc(userObj.id).set(user)
       .then(() => user)
   },
 
-  getUserByEmail (email) {
-    
+  getUser (uid) {
+    usersRef.doc(uid).get().then((doc) => {
+      if (doc && doc.exists) {
+        return doc.data()
+      }
+    })
+  },
+
+  deleteUser (uid) {
+    usersRef.doc(uid).update({
+      isActive: false
+    }).then(() => {
+      const msg = 'Document successfully updated!'
+      return msg
+    })
   }
+
 }
